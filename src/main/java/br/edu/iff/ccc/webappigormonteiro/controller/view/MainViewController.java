ed7559ca6aa8ba299ac384b2a30efbdf5b958280
IGOR MONTEIRO MAGRO <IGORMAGRO@corp.campos.rj.gov.br>
2025-08-12 12:49:50 -0300

    Adicionada rota e template de teste

--- src/main/java/br/edu/iff/ccc/webappigormonteiro/controller/view/MainViewController.java
+++ src/main/java/br/edu/iff/ccc/webappigormonteiro/controller/view/MainViewController.java
@@ -1,18 +1,16 @@
 package br.edu.iff.ccc.webappigormonteiro.controller.view;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
-@Controller 
+@Controller
 @RequestMapping(path = "/principal")
-public class MainViewController{
+public class MainViewController {
     @GetMapping()
     public String getHomePage() {
-        return "home.html";
+        // Render template src/main/resources/templates/home.html => view name "home"
+        return "home";
     }
-    
-
-
 
 }
